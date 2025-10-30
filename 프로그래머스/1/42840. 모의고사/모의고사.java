dd0498b0a792aefb2d2213bddaa4100523626224
hyen20719 <hyen20719@gmail.com>
2025-10-30 23:32:14 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] result = new int[3];
+        // 1번 수포자 
+        int[] person1 = new int[]{1, 2, 3, 4, 5};
+        // 2번 수포자
+        int[] person2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        // 3번 수포자 
+        int[] person3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        // 정답 갯수
+        for(int i =0; i<answers.length; i++){
+            if (answers[i] == person1[i % person1.length]){
+                result[0]++;
+            }
+            if (answers[i] == person2[i % person2.length]){
+                result[1]++;
+            }
+            if (answers[i] == person3[i % person3.length]){
+                result[2]++;
+            }
+        }
+        
+        // 가장 높은 점수 여러명인 경우 체크
+        int maxNum = Math.max(result[0],Math.max(result[1], result[2]));
+        int cnt = 0;
+        int[] max = new int[3];
+        
+        for(int i=0; i<result.length; i++){
+            if(maxNum == result[i]){
+                max[cnt] = i+1;
+                cnt++;
+            }
+        }
+        // 사이즈 조정
+        int[] resized = Arrays.copyOf(max, cnt);
+        return resized;
+    }
+    
+}
