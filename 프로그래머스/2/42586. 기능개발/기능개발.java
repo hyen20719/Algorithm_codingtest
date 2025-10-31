831b0bf1f904926f45fc3c716a2bcdf8021c33b3
hyen20719 <hyen20719@gmail.com>
2025-11-01 00:15:06 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        // 큐에 작업일수 담기
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++){
+            int cur = progresses[i];
+            int count = 0;
+            while(cur < 100 ){
+                cur += speeds[i];
+                count++;
+            }
+            q.offer(count);  
+        }
+        
+        // 큐 size로 배열 생성
+        answer = new int[q.size()];
+        int count = 0;
+        while(!q.isEmpty()){
+            int cnt = 1;
+            int cur = q.poll(); 
+           // System.out.println("현재===" + cur);
+            // 큐 안의 다음 기능들이 현재 기능과 함께 배포될 수 있는지 확인
+            while(!q.isEmpty() && q.peek() <= cur){ 
+                q.poll(); // 앞의 날짜보다 뒤의 날짜가 작으면 꺼내서 함께 배포
+                cnt++;
+            }
+            answer[count++] = cnt;
+        }
+       int[] answer2 =  Arrays.copyOf(answer,count);
+        return answer2;
+    }
+}
