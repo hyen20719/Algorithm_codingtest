1263008b92464fea746062061188893acd294520
hyen20719 <hyen20719@gmail.com>
2025-11-19 18:22:18 +0900

    [level 2] Title: 문자열 압축, Time: 38.03 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,46 +1,28 @@
 class Solution {
     public int solution(String s) {
+        int answer = 0;
         int len = s.length();
-        int answer = len;
-        
-        for(int i=1; i<=len/2; i++){
-            StringBuilder sb = new StringBuilder();
-            String str = s.substring(0, i);
-            int count = 1;
-            
-            for(int j=i; j<=len; j+=i){
-                int end = j+i;
-                if(end > len) end = len;
-                
-                String str2 = s.substring(j, end);
-                
-                if(str.equals(str2)){
-                   // System.out.println(str2);
-                    count++;
-                   // System.out.println(str);
-                }else{
-                    if(count > 1){
-                        sb.append(count);
-                    }
-                    sb.append(str);
-                    
-                    // 새조각으로
-                    str = str2;
-                    count = 1;
-                }
-                
-            }
-            
-             // 마지막 남은 조각 
-            if(count > 1){
-                sb.append(count);
-            }
-            sb.append(str);
-            
-            answer = Math.min(answer, sb.length());
+
+        for(int i=1; i<=(len/2)+1; i++){
+            int result = dfs(s, i, 1).length();
+            answer = i==1 ? result : (answer>result?result:answer);
         }
 
-        
         return answer;
+    }
+
+    public String dfs(String s, int n, int repeat){
+        if(s.length() < n) return s;
+        String result = "";
+        String pre = s.substring(0, n);
+        String post = s.substring(n, s.length());
+
+        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+        if(!post.startsWith(pre)){
+            if(repeat ==1) return result += pre + dfs(post, n, 1);
+            return result += Integer.toString(repeat) + pre + dfs(post, n, 1);
+        }
+
+        return result += dfs(post, n, repeat+1);
     }
 }
