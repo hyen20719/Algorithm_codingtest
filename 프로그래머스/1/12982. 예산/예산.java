b4f4214e104baf3b84cb10aaa01f0433f97bd154
hyen20719 <hyen20719@gmail.com>
2025-11-19 15:06:37 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,22 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int count = 0;
+        int cnt = 0;
         Arrays.sort(d);
-        Stack<Integer> stack = new Stack<>();
-        for(int i=d.length-1; i>=0; i--){
-            stack.push(d[i]);
-        }
         
-        while(!stack.isEmpty()){
-            int cur = stack.pop();
-            if (answer + cur <= budget) {
-                answer += cur;  // 예산을 초과하지 않으면 지원
-                count++;         // 지원한 부서 수 증가
-            } else {
-                break;           // 예산을 초과하면 더 이상 지원하지 않음
+        for(int i : d){
+            if(answer < budget){
+                answer +=i;
+                cnt++;
+            }
+            if(answer > budget){
+                answer -=i;
+                cnt--;
+            }
+            if(answer == budget){
+                break;
             }
         }
-        return count;
+        
+        return cnt;
     }
 }
