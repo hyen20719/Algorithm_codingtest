8061a2104fa165080ea6d0c016b77beabc498011
hyen20719 <hyen20719@gmail.com>
2025-11-20 15:57:28 +0900

    [level 1] Title: 신고 결과 받기, Time: 245.37 ms, Memory: 198 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,53 +2,52 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int len = id_list.length;
-        int[] answer = new int[len];
-        
-        // 신고자,[신고당한자 list]
-        Map<String, HashSet<String>> map = new HashMap<>();
-        // 신고 당한 사람, 횟수
-        Map<String, Integer> cntMap = new HashMap<>();
-        for(String id : id_list){
-            map.put(id, new HashSet<String>());
-            cntMap.put(id, 0);
+        int[] answer = new int[id_list.length];
+        ArrayList<User> users = new ArrayList<>();
+        HashMap<String,Integer> suspendedList = new HashMap<>(); //<이름>
+        HashMap<String,Integer> idIdx = new HashMap<String,Integer>(); // <이름, 해당 이름의 User 클래스 idx>
+        int idx = 0;
+
+        for(String name : id_list) {
+            idIdx.put(name,idx++);
+            users.add(new User(name));
         }
-        
-        for(int i=0; i<report.length; i++){
-            String[] srr = report[i].split(" ");
-            String reporter = srr[0]; //신고자
-            
-            if(map.containsKey(reporter)){
-                map.get(reporter).add(srr[1]);
-            }
+
+        for(String re : report){
+            String[] str = re.split(" ");
+            //suspendedCount.put(str[0], suspendedCount.getOrDefault(str[0],0)+1);
+            users.get( idIdx.get(str[0])).reportList.add(str[1]);
+            users.get( idIdx.get(str[1])).reportedList.add(str[0]);
         }
-        
-        // 신고당한 사람, 횟수
-        for(String reporter : map.keySet()){
-            for(String target : map.get(reporter)){
-                cntMap.put(target, cntMap.get(target)+1);
-            }
+
+        for(User user : users){
+            if(user.reportedList.size() >= k)
+                suspendedList.put(user.name,1);
         }
-        
-        // k번 이상 신고당한 사람 집합
-        HashSet<String> set = new HashSet<>();
-        for(String id : id_list){
-            if(cntMap.get(id) >= k){
-                set.add(id);
-            }
+
+         for(User user : users){
+             for(String nameReport : user.reportList){
+                 if(suspendedList.get(nameReport) != null){
+                     answer[idIdx.get(user.name)]++;
+                 }
+
+             }
         }
-        
-        // 각 사용자가 신고한 사람 중 정지된 사람 수 세기
-        for(int i=0; i<len; i++){
-            int count = 0;
-            for(String target : map.get(id_list[i])){
-                if(set.contains(target)){
-                    count++;
-                }
-            }
-            answer[i] = count;
-        }
-        
+
+
+
+
         return answer;
+    }
+}
+
+class User{
+    String name;
+    HashSet<String> reportList;
+    HashSet<String> reportedList;
+    public User(String name){
+        this.name = name;
+        reportList = new HashSet<>();
+        reportedList = new HashSet<>();
     }
 }
