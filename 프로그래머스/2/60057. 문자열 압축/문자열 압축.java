87608206c70c9552002fef1ad45f0d6e73c64e9b
hyen20719 <hyen20719@gmail.com>
2025-11-20 13:40:46 +0900

    [level 2] Title: 문자열 압축, Time: 4.54 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,28 +1,41 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
+        
         int len = s.length();
-
-        for(int i=1; i<=(len/2)+1; i++){
-            int result = dfs(s, i, 1).length();
-            answer = i==1 ? result : (answer>result?result:answer);
+        int answer = len;
+        
+        for(int i=1; i<len; i++){
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            String str = s.substring(0,i);
+           // System.out.println(str);
+            for(int j=i; j<len; j+=i){
+                int end = j+i >len ? len : j+i;
+                String str2 = s.substring(j,end);
+                //System.out.println("str2 : " + str2);
+                
+                if(str.equals(str2)){ // 같은 경우
+                    count++;
+                }else{
+                    if(count >1){
+                        sb.append(count);
+                    }
+                    sb.append(str);
+                    count = 1;
+                }
+                str = str2;
+            }
+            // 문자열의 마지막 부분
+            if(count > 1){
+                sb.append(count);
+            }
+            sb.append(str);
+            
+            answer = Math.min(answer, sb.length());
+            
         }
-
+        
+        
         return answer;
-    }
-
-    public String dfs(String s, int n, int repeat){
-        if(s.length() < n) return s;
-        String result = "";
-        String pre = s.substring(0, n);
-        String post = s.substring(n, s.length());
-
-        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
-        if(!post.startsWith(pre)){
-            if(repeat ==1) return result += pre + dfs(post, n, 1);
-            return result += Integer.toString(repeat) + pre + dfs(post, n, 1);
-        }
-
-        return result += dfs(post, n, repeat+1);
     }
 }
