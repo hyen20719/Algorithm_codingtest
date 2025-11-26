435d8ff62a708fe0d8a690a77156677e005b939c
hyen20719 <hyen20719@gmail.com>
2025-11-26 15:47:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.28 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -2,44 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(String dartResult) {
-        int answer = 0; // 현재숫자
-        
+        int answer = 0;
+        // S : 1제곱 , D : 2제곱 , T : 3제곱 
+        // * : 점수2배 (이전점수도 2배) , # : -2배
         Stack<Integer> stack = new Stack<>();
-        
         for(int i=0; i<dartResult.length(); i++){
             char c = dartResult.charAt(i);
             
             if(Character.isDigit(c)){
-                answer = answer * 10 + (c - '0');
-                
-            }else if(c == 'S' || c == 'D' || c == 'T' ){
+                if(i != 0 && Character.isDigit(dartResult.charAt(i-1))){
+                    int pre = stack.pop();
+                    //System.out.println(pre);
+                    stack.push(pre * 10 + (c-'0'));
+                }else{
+                    stack.push(c - '0');
+                }
+               // System.out.println(stack);
+            }else if(c == 'S' || c == 'D' ||c == 'T'){
+                int cur = stack.pop();
                 switch(c){
-                    case 'S':answer = (int)Math.pow(answer,1); break;
-                    case 'D':answer = (int)Math.pow(answer,2); break;
-                    case 'T':answer = (int)Math.pow(answer,3); break;
+                    case 'S' : stack.push((int)Math.pow(cur,1)); break;
+                    case 'D' : stack.push((int)Math.pow(cur,2)); break;
+                    case 'T' : stack.push((int)Math.pow(cur,3)); break;
                 }
-                stack.push(answer); // 계산한 현재값
-                answer = 0; // 다음 점수를 위해
-            }else if(c == '*'){
-                int num = stack.pop(); // S,D,T에서 계산한 점수 가져오기
-                num *= 2; // 현재 점수 2배
-                if(!stack.isEmpty()){ //스택 비어있지않으면 이전 점수도 
-                    int prev = stack.pop() *2;
-                    stack.push(prev);
+                //System.out.println("ddd" + stack);
+            }else{
+                int cur = stack.pop();
+                if(c == '*'){ // 2배
+                    if(!stack.isEmpty()){
+                        stack.push(stack.pop()*2);
+                    }
+                    stack.push(cur*2);
+                }else{ // 마이너스
+                    stack.push(cur*-1);
                 }
-                stack.push(num);
-                // 이전 점수 먼저 push하고 그 다음 점수
-            }else if(c == '#'){
-                int num = stack.pop() * -1;
-                stack.push(num);
             }
         }
         
-        // 최종 점수 합 계산
-        answer = 0; 
-        for(int score : stack){
-            answer += score;
+        while(!stack.isEmpty()){
+            answer += stack.pop();
         }
+        
         
         return answer;
     }
