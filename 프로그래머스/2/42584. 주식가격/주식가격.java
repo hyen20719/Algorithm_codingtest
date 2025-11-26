f5b6873b1e27f9a720990986b1b07c26d6743d99
hyen20719 <hyen20719@gmail.com>
2025-11-26 15:16:35 +0900

    [level 2] Title: 주식가격, Time: 47.87 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,25 +4,25 @@ class Solution {
     public int[] solution(int[] prices) {
         int len = prices.length;
         int[] answer = new int[len];
-        Stack<Integer> stack = new Stack<>();
         
+        Stack<Integer> stack = new Stack<>();
         for(int i=0; i<len; i++){
-            // 가격이 떨어진 지점 찾기
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){ 
-                int idx = stack.pop();
-                answer[idx] = i - idx; // 떨어진 지점에 길이 넣기
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                int idx = stack.pop(); 
+                //System.out.println(i);
+                answer[idx] = i - idx; // 현재값 - 이전값
             }
             stack.push(i);
         }
         
-       // System.out.println(stack); // [0,1,3,4]
-        
         while(!stack.isEmpty()){
-            int idx = stack.pop();
-            answer[idx] = len - idx -1; // 가격이 끝날때까지 떨어지지않은 시간
+            int cur = stack.pop();
+          //  System.out.println(cur);
+            answer[cur] = len - cur -1;
         }
         
-        
+        //System.out.println(stack);
+        //System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
