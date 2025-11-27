fc2897dd66fed51887500006812d95b313238698
hyen20719 <hyen20719@gmail.com>
2025-11-27 17:11:11 +0900

    [level 2] Title: 타겟 넘버, Time: 6.97 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,26 @@
 class Solution {
-    static int answer = 0;
+    static int[] numbers;
+    static int target;
+    static int answer;
+    
     public int solution(int[] numbers, int target) {
+        this.answer = 0;
+        this.numbers = numbers;
+        this.target = target;
         
-        dfs(numbers, 0, 0, target);
+        dfs(0,0); // index, sum
         return answer;
     }
     
-    public void dfs(int[] numbers, int idx, int sum, int target){
-        if(idx == numbers.length){
+    public void dfs(int index, int sum){
+         if(index == numbers.length){
             if(sum == target){
                 answer++;
             }
              return;
-        }
-        
-       dfs(numbers, idx+1, sum+numbers[idx], target);
-       dfs(numbers, idx+1, sum-numbers[idx], target);
-        
+         }
+       // System.out.println(index + " , " + sum);
+        dfs(index+1, sum+numbers[index]);
+        dfs(index+1, sum-numbers[index]);
     }
 }
