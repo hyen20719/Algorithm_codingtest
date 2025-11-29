1917a7778355864e8aef654da051dd2bde50de67
hyen20719 <hyen20719@gmail.com>
2025-11-29 16:11:33 +0900

    [level 2] Title: 소수 찾기, Time: 73.74 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,49 +1,52 @@
-import java.util.*;
-
 class Solution {
-    static boolean[] visited;
-    static HashSet<Integer> set = new HashSet<>();
     static String numbers;
-
+    static boolean[] v; // 방문 여부
+    static boolean[] checked; // 이미 계산한 숫자 여부
+    static int answer;
+    
     public int solution(String numbers) {
+        answer = 0;
         this.numbers = numbers;
-        visited = new boolean[numbers.length()];
+        v = new boolean[numbers.length()];
+        checked = new boolean[100000000];
         
-        dfs("", 0);   // 빈 문자열부터 시작
-        
-        // set 안에는 소수만 들어 있음
-        int count = 0;
-        for (int n : set) {
-            if (isPrime(n)) count++;
-        }
-        return count;
+        dfs("", 0);
+        return answer;
     }
     
-    public void dfs(String current, int depth) {
-        // current가 비어 있지 않으면 숫자로 변환해 set에 추가
-        if (!current.equals("")) {
-            set.add(Integer.parseInt(current));
+    public void dfs(String cur, int depth){
+        // 숫자 조합이 비어있지않을때 소수 판별
+        if(!cur.equals("")){
+            int num = Integer.parseInt(cur);
+            if(!checked[num] && isPrime(num)){ // 중복 체크 같이
+                checked[num] = true;
+                answer++;
+            }
         }
-
-        // 더 이상 만들 수 없는 경우 종료
-        if (depth == numbers.length()) return;
         
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                
-                dfs(current + numbers.charAt(i), depth + 1);
-                
-                visited[i] = false;   // 백트래킹
+        if(depth == numbers.length()){
+            return;
+        }
+        
+        
+        for(int i=0; i<numbers.length(); i++){
+            if(!v[i]){
+                v[i] = true;
+                dfs(cur+numbers.charAt(i), depth+1);
+                v[i] = false; // 다른 조합 탐색을 위해 복원
             }
         }
     }
-
-    public static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    
+    // 소수 찾기
+    public boolean isPrime(int num){
+        boolean chk = true;
+        if(num < 2){chk = false; return chk;}
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0){
+                return false;
+            }
         }
-        return true;
+        return chk;
     }
 }
