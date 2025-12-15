74fd3a89f47087d8af2c8d7e9c3659cba9f4f197
hyen20719 <hyen20719@gmail.com>
2025-12-15 14:21:09 +0900

    [level 2] Title: 전화번호 목록, Time: 393.13 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,22 +3,16 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        //중복없으니 hashset으로
-        HashSet<String> set = new HashSet<>();
-        for(String p : phone_book){
-            set.add(p);
-        }
+        Arrays.sort(phone_book);
         
-        for(String phone : phone_book){
-            for(int i=1; i<phone.length(); i++){
-                String prefix = phone.substring(0, i); // 전화번호 길이-1이라 같은번호로 체크하는 경우 x
-                
-                if(set.contains(prefix)){ // 접두어인 경우
-                    return answer = false;
-                }
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
             }
         }
         
         return answer;
+        
+        
     }
 }
