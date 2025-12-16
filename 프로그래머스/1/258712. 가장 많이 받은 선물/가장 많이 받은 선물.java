f6d2d6e0c9ec9994a3056cc1abb888885d1e3380
hyen20719 <hyen20719@gmail.com>
2025-12-16 17:21:58 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 13.76 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -4,65 +4,41 @@ class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
         int len = friends.length;
+        int[][] gift = new int[len][len]; // [선물준사람][받은사람]
+        int[] score = new int[len]; // 각 선물지수
         
-        Map<String,Integer> idxMap = new HashMap<>();
-        Map<String,Map<String, Integer>> giftMap = new HashMap<>();
-        Map<String,Integer> levelMap = new HashMap<>();
-        
-        // 사람리스트
+        Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<len; i++){
-            idxMap.put(friends[i], i);
-            levelMap.put(friends[i], 0);
+            map.put(friends[i], i);
         }
         
-        // 선물 준사람 (받은사람 :갯수)
-        for(String gift : gifts){
-            String[] g = gift.split(" ");
-            String giver = g[0];
-            String receiver = g[1];
+        for(String g : gifts){
+            String[] s = g.split(" ");
+            int giver = map.get(s[0]);
+            int receiver = map.get(s[1]);
             
-            giftMap.putIfAbsent(giver, new HashMap<>());
-            Map<String, Integer> recevMap = giftMap.get(giver);
-            recevMap.put(receiver, recevMap.getOrDefault(receiver,0)+1);
-            //선물 지수
-            levelMap.put(giver, levelMap.get(giver)+1);
-            levelMap.put(receiver, levelMap.get(receiver)-1);
-            
+            gift[giver][receiver]++;
+            score[giver]++;
+            score[receiver]--;
         }
-       // System.out.println(giftMap);
         
-        // 다음 달 선물 갯수
         int[] nextGift = new int[len];
-        
-        // 친구 쌍 비교
         for(int i=0; i<len; i++){
-            for(int j=i+1; j<len; j++){
-                String a = friends[i];
-                String b = friends[j];
-                
-                // a에게 아무도 안 준경우를 대비해서 getOrDefault(해시맵 생성) 사용
-                // a가 b에게 안 준 경우 내부 map에 없기때문에 0으로 처리
-                int aToB = giftMap.getOrDefault(a, new HashMap<>()).getOrDefault(b,0);
-                int bToA = giftMap.getOrDefault(b, new HashMap<>()).getOrDefault(a,0);
-                
-               // System.out.println(a+", " + b + ", " + aToB);
-                
-                if(aToB > bToA){ // a가 더 많이 준 경우
+            for(int j=i+1; j<len; j++){ // 앞사람은 이미 비교했기때문에 i 다음 사람부터
+                if(gift[i][j] > gift[j][i]){ // i가 더 많이 준 경우
                     nextGift[i]++;
-                }else if(aToB < bToA){ //b가 더 많이 준 경우
-                    nextGift[j]++;   
-                }else{
-                    // 선물 준 횟수 동일시 선물지수로 비교
-                    if(levelMap.get(a) > levelMap.get(b)){
+                }else if(gift[i][j] < gift[j][i]){ // j가 더 많이 준 경우
+                    nextGift[j]++;
+                }else{ // 선물 지수로 비교
+                    if(score[i] > score[j]){
                         nextGift[i]++;
-                    }else if(levelMap.get(a) < levelMap.get(b)){
+                    }else if(score[i] < score[j]){
                         nextGift[j]++;
                     }
+                    
                 }
             }
         }
-        
-        //System.out.println(Arrays.toString(nextGift));
         
         for(int n : nextGift){
             answer = Math.max(answer, n);
