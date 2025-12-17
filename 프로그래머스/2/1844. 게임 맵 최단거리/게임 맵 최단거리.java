923a94857aa83aa3df3a1748f1cd27b443f078d8
hyen20719 <hyen20719@gmail.com>
2025-12-17 15:41:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.29 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,21 +1,23 @@
 import java.util.*;
 
 class Solution {
-    static int[][] maps;
-    static boolean[][] v; 
+    int[][] maps;
+    boolean[][] v;
     
     public int solution(int[][] maps) {
+        int answer = 0;
         this.maps = maps;
+        
         int n = maps.length;
         int m = maps[0].length;
+        
         v = new boolean[n][m];
         
         int[] dx = {1,-1,0,0};
         int[] dy = {0,0,1,-1};
         
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0,0,1}); // x,y, distance
-        v[0][0] = true;
+        q.add(new int[]{0,0,1}); // x,y,dist
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
@@ -23,28 +25,25 @@ class Solution {
             int y = cur[1];
             int dist = cur[2];
             
-            // 도착지점
             if(x == n-1 && y == m-1){
                 return dist;
             }
             
-            // 4방향 탐색
             for(int i=0; i<4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx >= 0 && ny>= 0 && nx < n && ny < m){
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
                     if(!v[nx][ny] && maps[nx][ny] == 1){
-                        v[nx][ny] = true;
-                        q.add(new int[]{nx, ny, dist+1});
-                    }
+                        v[nx][ny] = true; // 큐에 넣을때 방문 처리
+                        q.offer(new int[]{nx,ny,dist+1});
+                    }    
                 }
+                
             }
             
         }
         
         return -1;
     }
-    
-   
 }
