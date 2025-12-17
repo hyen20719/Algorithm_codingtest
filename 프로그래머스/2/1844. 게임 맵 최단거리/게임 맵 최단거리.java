fa787f6bd3345c0c90c02454a6c8d1b3ef255fd0
hyen20719 <hyen20719@gmail.com>
2025-12-17 15:47:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.78 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,41 +2,33 @@ import java.util.*;
 
 class Solution {
     int[][] maps;
-    boolean[][] v;
     
     public int solution(int[][] maps) {
-        int answer = 0;
+        // 방문 체크없이 바로 거리 계산하는 방식
         this.maps = maps;
         
         int n = maps.length;
         int m = maps[0].length;
         
-        v = new boolean[n][m];
-        
         int[] dx = {1,-1,0,0};
         int[] dy = {0,0,1,-1};
         
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,1}); // x,y,dist
+        q.add(new int[]{0,0}); // x,y
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-            int dist = cur[2];
-            
-            if(x == n-1 && y == m-1){
-                return dist;
-            }
             
             for(int i=0; i<4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(!v[nx][ny] && maps[nx][ny] == 1){
-                        v[nx][ny] = true; // 큐에 넣을때 방문 처리
-                        q.offer(new int[]{nx,ny,dist+1});
+                    if(maps[nx][ny] == 1){
+                        maps[nx][ny] = maps[x][y] +1; // 큐에 넣을때 방문 처리
+                        q.offer(new int[]{nx,ny});
                     }    
                 }
                 
@@ -44,6 +36,7 @@ class Solution {
             
         }
         
-        return -1;
+        int answer = maps[n-1][m-1];
+        return answer == 1 ? -1 : answer;
     }
 }
