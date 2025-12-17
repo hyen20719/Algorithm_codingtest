1919092d806c917d583fea35e14a9b000484ee14
hyen20719 <hyen20719@gmail.com>
2025-12-17 14:56:48 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,26 +3,35 @@ import java.util.*;
 class Solution {
     int[][] computers;
     boolean[] v;
+    
     public int solution(int n, int[][] computers) {
+        int answer = 0;
         this.computers = computers;
         v = new boolean[n];
         
-        int answer = 0;
         for(int i=0; i<n; i++){
             if(!v[i]){
-                dfs(i);   
+                bfs(i);
                 answer++;
             }
         }
+        
         return answer;
     }
     
-    public void dfs(int cur){
-        v[cur] = true;
+    public void bfs(int st){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(st);
+        v[st] = true;
         
-        for(int i=0; i<computers.length; i++){
-            if(computers[cur][i] == 1 && !v[i]){
-                dfs(i);
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int i=0; i<computers.length; i++){
+                if(!v[i] && computers[cur][i] == 1){
+                    v[i] = true;
+                    q.offer(i);
+                }
             }
         }
     }
