98e7d29d80bd8cc8b65842fd634221f7c13f84d0
hyen20719 <hyen20719@gmail.com>
2025-12-17 17:13:28 +0900

    [level 3] Title: 단어 변환, Time: 0.42 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,56 +1,48 @@
 import java.util.*;
 
 class Solution {
-    static String begin;
-    static String target;
-    static String[] words;
-    static boolean[] v; //방문한 단어 추적하는 배열
-    static int answer = Integer.MAX_VALUE;
+    String[] words;
+    String target;
+    boolean[] v;
+    int answer = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
-        this.begin = begin;
-        this.target = target;
         this.words = words;
+        this.target = target;
         v = new boolean[words.length];
         
         if(!Arrays.asList(words).contains(target)){
             return 0;
         }
         
-        // begin과 현재 변환 횟수로 시작
         dfs(begin, 0);
         
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
     
-    public void dfs(String w, int count){
-        // 새로운 begin이 target과 같을때 종료
+    public void dfs(String w, int cnt){
+        
         if(w.equals(target)){
-            answer = Math.min(answer, count);
+            answer = Math.min(answer, cnt);
             return;
         }
         
         for(int i=0; i<words.length; i++){
-            // 방문하지 않았을때
             if(!v[i]){
                 String word = words[i];
-                int cnt = 0; // 두 단어가 한글자만 다른지 확인
-                
+            
+                int count = 0;
                 for(int j=0; j<word.length(); j++){
-                    if(w.charAt(j) != word.charAt(j)){
-                        cnt++;
+                    if(word.charAt(j) != w.charAt(j)){
+                        count++;
                     }
                 }
-
-                if(cnt == 1){
+                if(count == 1){
                     v[i] = true;
-                    dfs(word, count+1);
+                    dfs(word, cnt+1);
                     v[i] = false;
-                }    
+                }
             }
-            
         }
     }
-    
-    
 }
