eb7487a2b2d4b55d09520e5ebb9979b5293b9a49
hyen20719 <hyen20719@gmail.com>
2025-12-18 16:50:02 +0900

    [level 3] Title: 여행경로, Time: 0.73 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,34 +1,50 @@
 import java.util.*;
 
+import java.util.*;
+
 class Solution {
-    static Map<String, PriorityQueue<String>> map;
-    static List<String> answer;
-    
+    String[][]  tickets;
+    String[] answer;
+    boolean[] v;
+    boolean found = false;
+        
     public String[] solution(String[][] tickets) {
-        answer = new ArrayList<>();
-        map = new HashMap<>();
+        this.tickets = tickets;
+        int n = tickets.length;
+        v = new boolean[n];
         
-        // 출발지, [도착지] 큐삽입
-        for(String[] t : tickets){
-            map.putIfAbsent(t[0], new PriorityQueue<>());
-            map.get(t[0]).offer(t[1]);
-        }
+        Arrays.sort(tickets, (a,b)->{
+            if(a[0].equals(b[0])){
+                return a[1].compareTo(b[1]);
+            }
+                return a[0].compareTo(b[0]);
+        });
         
-        //System.out.println(map);
-        dfs("ICN");
+        answer = new String[n+1];
+        answer[0] = "ICN";
+        dfs("ICN", 0);
         
-        return answer.toArray(new String[0]);
+        return answer;
     }
     
-    public void dfs(String ticket){
-        PriorityQueue<String> pq = map.get(ticket);
+    public void dfs(String cur, int cnt){
+        if(found) return;
         
-        while(pq != null && !pq.isEmpty()){
-            String next = pq.poll();
-            dfs(next);
+        if(cnt == tickets.length){
+            found = true;
+            return;
         }
         
-        answer.add(0, ticket);
-       // System.out.println(answer);
+        for(int i=0; i<tickets.length; i++){
+            if(!v[i] && tickets[i][0].equals(cur)){
+                v[i] = true;
+                answer[cnt+1] = tickets[i][1];
+                dfs(tickets[i][1], cnt+1);
+                
+                if(!found){
+                    v[i] = false;
+                }
+            }
+        }
     }
 }
