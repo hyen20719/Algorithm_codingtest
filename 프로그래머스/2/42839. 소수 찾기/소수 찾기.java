30c5d70498859a4cba6ec409c328a9e3a0d37285
hyen20719 <hyen20719@gmail.com>
2025-12-19 17:48:11 +0900

    [level 2] Title: 소수 찾기, Time: 18.55 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,52 +1,45 @@
+import java.util.*;
+
 class Solution {
-    static String numbers;
-    static boolean[] v; // 방문 여부
-    static boolean[] checked; // 이미 계산한 숫자 여부
-    static int answer;
+    String numbers;
+    int len;
+    boolean[] v;
+    Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
-        answer = 0;
+        int answer = 0;
         this.numbers = numbers;
-        v = new boolean[numbers.length()];
-        checked = new boolean[100000000];
+        len = numbers.length();
+        v = new boolean[len];
         
-        dfs("", 0);
-        return answer;
+        dfs("");
+        
+        return set.size();
     }
     
-    public void dfs(String cur, int depth){
-        // 숫자 조합이 비어있지않을때 소수 판별
+    public void dfs(String cur){
+        
         if(!cur.equals("")){
             int num = Integer.parseInt(cur);
-            if(!checked[num] && isPrime(num)){ // 중복 체크 같이
-                checked[num] = true;
-                answer++;
+            if(isPrime(num)){
+                set.add(num);
             }
         }
         
-        if(depth == numbers.length()){
-            return;
-        }
-        
-        
-        for(int i=0; i<numbers.length(); i++){
+        for(int i=0; i<len; i++){
             if(!v[i]){
                 v[i] = true;
-                dfs(cur+numbers.charAt(i), depth+1);
-                v[i] = false; // 다른 조합 탐색을 위해 복원
+                dfs(cur+numbers.charAt(i));
+                v[i] = false;
             }
         }
     }
     
-    // 소수 찾기
-    public boolean isPrime(int num){
-        boolean chk = true;
-        if(num < 2){chk = false; return chk;}
-        for(int i=2; i*i<=num; i++){
-            if(num % i == 0){
-                return false;
-            }
+    boolean isPrime(int n){
+        if( n < 2) return false;
+        for(int i=2; i*i<=n; i++){
+            if(n % i == 0) return false;
         }
-        return chk;
+        return true;
     }
 }
