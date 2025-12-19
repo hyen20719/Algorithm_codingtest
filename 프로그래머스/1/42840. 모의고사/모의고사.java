b9ebd7fe719cfaa961bb63b32e7c953d92b93a2d
hyen20719 <hyen20719@gmail.com>
2025-12-19 16:21:36 +0900

    [level 1] Title: 모의고사, Time: 4.49 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,41 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] result = new int[3];
-        // 1번 수포자 
-        int[] person1 = new int[]{1, 2, 3, 4, 5};
-        // 2번 수포자
-        int[] person2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        // 3번 수포자 
-        int[] person3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        ArrayList<Integer> list = new ArrayList<>();
+        int maxCnt = 0; // 가장 높은 점수
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 정답 갯수
-        for(int i =0; i<answers.length; i++){
-            if (answers[i] == person1[i % person1.length]){
-                result[0]++;
-            }
-            if (answers[i] == person2[i % person2.length]){
-                result[1]++;
-            }
-            if (answers[i] == person3[i % person3.length]){
-                result[2]++;
-            }
-        }
+        int c1 = cntChk(answers, p1);
+        int c2 = cntChk(answers, p2);
+        int c3 = cntChk(answers, p3);
+        maxCnt = Math.max(c1, Math.max(c2,c3));
         
-        // 가장 높은 점수 여러명인 경우 체크
-        int maxNum = Math.max(result[0],Math.max(result[1], result[2]));
-
-        // 동점자도 고려하여 list로 저장
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<result.length; i++){
-            if(result[i] == maxNum){
-                list.add(i+1);
-            }
-        }
+        if(maxCnt == c1){list.add(1);}
+        if(maxCnt == c2){list.add(2);}
+        if(maxCnt == c3){list.add(3);}
+        
         
         return list.stream().mapToInt(Integer::intValue).toArray();
-        
-        
     }
     
+    public int cntChk(int[] answers, int[] p){
+        int count = 0;
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == p[i%p.length]){
+                count++;
+            }
+        }
+        return count;
+    }
 }
