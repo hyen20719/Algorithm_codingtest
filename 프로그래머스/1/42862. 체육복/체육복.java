d5b832201e80be52130a5222624625b559454cbc
hyen20719 <hyen20719@gmail.com>
2025-12-23 14:42:32 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,40 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        boolean[] used = new boolean[reserve.length]; //reserve 학생 사용여부 
-        // 여벌이 있지만 도난도 당한 학생 처리 
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    lost[i] = -1; // 해결된 도난
-                    used[j] = true;
-                    break;
-                }
-            }
+        int[] p = new int[n+1];
+        for(int r : reserve){
+            p[r]++;
+        }
+        for(int l : lost){
+            p[l]--;
         }
         
-        // 남은 학생끼리 옷 빌리기
-        for(int i=0; i<lost.length; i++){
-            if(lost[i] == -1) continue; // 이미 해결된 학생
-            
-            boolean chk = false;
-            for(int j=0; j<reserve.length; j++){
-                if(used[j]) continue; // 이미 빌려준 reserve
-                
-                if(reserve[j] == lost[i]-1 || reserve[j] == lost[i]+1 ){
-                    used[j] = true;
-                    chk = true;
-                    break;
-                    
-                }
+        //System.out.println(Arrays.toString(p));
+        // lost에 사람이 남아있으면 그 수만큼 n-lost.length
+        for(int i=1; i<p.length-1; i++){
+            if(p[i] == 1 && p[i+1] == -1){
+                p[i+1]++;
+                p[i]--;
+               // continue;
+            }else if(p[i] == -1 && p[i-1] == 1){
+                p[i]++; 
+                p[i-1]--;
+            }else if(p[i] == -1 && p[i+1] == 1){
+                p[i]++; 
+                p[i+1]--;
             }
-            
-            if(!chk) answer--; // 못빌렸을 경우
         }
-        
+       // System.out.println(Arrays.toString(p));
+        for(int i : p){
+            if(i == -1){answer--;}
+        }
         return answer;
     }
 }
