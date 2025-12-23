e3bbdf4ee3bc1380f6249650f7b9a3d3d71d1de5
hyen20719 <hyen20719@gmail.com>
2025-12-23 16:10:55 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -4,28 +4,24 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         char[] crr = name.toCharArray();
-        int len = crr.length;
+        int len = name.length();
         
-        // 상하이동 
         for(char c : crr){
-            answer += Math.min(c- 'A', 'Z'-c+1);
+            answer += Math.min(c-'A', 'Z'-c+1);
         }
         
-        // 좌우이동
         int move = len-1;
         for(int i=0; i<len; i++){
-            int next = i+1;
-            
-            // 다음 문자부터 연속된 A구간
-            while(next < len && crr[next] == 'A'){
-                next++;
-                 
+            int cnt = i+1;
+            while(len > cnt && name.charAt(cnt) == 'A'){
+                cnt++;
             }
-            move = Math.min(move, i*2 + (len-next));
-            move = Math.min(move, (len-next)*2+i);
+            
+            move = Math.min(move, i*2+(len-cnt));
+            move = Math.min(move, (len-cnt)*2+i);
+            
         }
-       // System.out.println(answer);
         
-        return answer + move;
+        return answer+move;
     }
 }
