c774b1e22dbbcfb0b8c4003b1d2dcfc7e37dce83
hyen20719 <hyen20719@gmail.com>
2025-12-24 14:55:24 +0900

    [level 2] Title: 구명보트, Time: 25.19 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,20 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        // 정렬 후 양쪽 접근이 가능한 Deque 사용
         Arrays.sort(people);
-        int len = people.length;
-        int left = 0;
-        int right = len-1;
+        Deque<Integer> q = new ArrayDeque<>();
         
-        while(left <= right){
-            if(people[left] +people[right] <= limit){
-                answer++;
-                left++;
-                right--;
-            }else{
-                right--;
-                answer++;
+        for(int p : people) { 
+            q.offer(p);
+        }
+        //System.out.println(q);
+        
+        while(!q.isEmpty()){
+            int cur = q.pollLast();
+            if(!q.isEmpty() && cur + q.peekFirst() <= limit){
+                q.pollFirst();
             }
+            answer++;
         }
         
         return answer;
