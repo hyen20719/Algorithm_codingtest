62b429a0d448374f3a924e19b1c3f7df07fa08d7
hyen20719 <hyen20719@gmail.com>
2025-12-27 14:43:33 +0900

    [level 3] Title: 단속카메라, Time: 8.68 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        Arrays.sort(routes, (a,b)-> a[1]-b[1]);
         int answer = 0;
-        
-        // 끝 위치 기준 정렬
-        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
-        
-        int cam = Integer.MIN_VALUE; // 현재 카메라 위치
+        int cam = Integer.MIN_VALUE;
         
         for(int[] route : routes){
-            int start = route[0];
-            int end = route[1];
-            
-            if(cam < start){ // 카메라로 이 차량을 커버 못하면
-                cam = end;   // 끝 위치에 카메라 설치
-                System.out.println(cam);
+            int st = route[0]; 
+            int ed = route[1]; 
+            //System.out.println(st + " , " + cam);
+            if(st > cam){
+                cam = ed; 
+                //System.out.println(cam);
                 answer++;
             }
         }
