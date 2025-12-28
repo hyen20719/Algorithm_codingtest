fba9db5c293bc7f277a96ea47bf7123539d8de28
hyen20719 <hyen20719@gmail.com>
2025-12-28 15:03:00 +0900

    [level 3] Title: 정수 삼각형, Time: 10.06 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,33 +1,15 @@
 import java.util.*;
 
 class Solution {
-    int[][] triangle;
-    int[][] dp;
-    int len;
-    
     public int solution(int[][] triangle) {
         int answer = 0;
-        this.triangle = triangle;
-        len = triangle.length;
-        dp = new int[len][len];
-        
-        return dfs(0,0);
-    }
-    
-    public int dfs(int depth, int idx){
-        // 맨 아래 도착
-        if(depth == len-1){
-            return triangle[depth][idx];
+        // Bottom-Up DP
+        for(int i = triangle.length-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                triangle[i][j] += Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+            }
         }
         
-        // 이미 계산된 경우
-        if(dp[depth][idx] != 0 ){
-            return dp[depth][idx];
-        }
-        
-        dp[depth][idx] = triangle[depth][idx] +
-                         Math.max(dfs(depth+1, idx), dfs(depth+1,idx+1));
-        return dp[depth][idx];
-        
+        return triangle[0][0];
     }
 }
