2a816cc07cf2c66a451d080476836ca4218a0678
hyen20719 <hyen20719@gmail.com>
2026-01-27 20:36:06 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,21 +1,26 @@
-import java.util.*;
-
 class Solution {
+    int count = 0; // 합이 0인 삼총사 개수
+    int[] numbers;
+
     public int solution(int[] number) {
-        int answer = 0;
-        int len = number.length; 
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int i=0; i<len; i++){
-            for(int j=i+1; j<len; j++){
-                for(int z=j+1; z<len; z++){
-                    if(number[i]+number[j]+number[z] == 0){
-                        answer++;   
-                    }
-                }
-            }
+        this.numbers = number;
+        dfs(0, 0, 0);
+        return count;
+    }
+
+    // idx: 현재 위치, selected: 선택한 개수, sum: 현재까지 합
+    private void dfs(int idx, int selected, int sum) {
+        if (selected == 3) { // 3개 선택 완료
+            if (sum == 0) count++;
+            return;
         }
-        
-        return answer;
+
+        if (idx == numbers.length) return; // 끝까지 갔는데 3개 못 고름
+
+        // 1) numbers[idx] 선택
+        dfs(idx + 1, selected + 1, sum + numbers[idx]);
+
+        // 2) numbers[idx] 선택 안함
+        dfs(idx + 1, selected, sum);
     }
 }
