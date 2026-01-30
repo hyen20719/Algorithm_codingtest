49f42f9ec474eac1ad93f084f775ccbbace3f272
hyen20719 <hyen20719@gmail.com>
2026-01-30 20:31:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,17 +1,19 @@
 import java.util.*;
-
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
+        
         int len = s.length();
-    
+        boolean answer = false;
+        
+        if(len == 4 || len == 6) answer = true;
+        
         for(char c : s.toCharArray()){
+            //System.out.println(c);
             if(!Character.isDigit(c)){
-                answer = false;
-                return answer;
+                return false;
             }
         }
         
-        return len == 4 || len == 6 ? answer : false;
+        return answer;
     }
 }
