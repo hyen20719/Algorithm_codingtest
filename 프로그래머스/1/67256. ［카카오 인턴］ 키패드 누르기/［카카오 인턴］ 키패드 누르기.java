b72218658c025aa450f8318b80a28ab7c6cd28a2
hyen20719 <hyen20719@gmail.com>
2026-02-05 14:53:08 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.30 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,68 +1,55 @@
 import java.util.*;
 
 class Solution {
-    // 숫자를 좌표로 바꾸는 함수
-    int[] getpos(int n){
-        if(n == 0) return new int[]{3,1};
-        if(n == 10) return new int[]{3,0}; //*위치
-        if(n == 11) return new int[]{3,2}; //#위치
-        return new int[]{(n-1)/3 ,(n-1)%3};
-    }
-    // 거리 계산
-        int dist(int a, int b){
-            int[] A = getpos(a);
-            int[] B = getpos(b);
-            return Math.abs(A[0]-B[0]) + Math.abs(A[1]-B[1]);
-        }
+    String hand;
+    StringBuilder sb = new StringBuilder();
+    int left = 10;
+    int right = 12;
     
     public String solution(int[] numbers, String hand) {
         String answer = "";
-        // [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
-        //  L  R  L  L  L  R  L  L  R  R  L
+        this.hand = hand;
         
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1,"L");
-        map.put(4,"L");
-        map.put(7,"L");
-        map.put(3,"R");
-        map.put(6,"R");
-        map.put(9,"R");
-        
-        int rcur=10, lcur=11; // 0으로 초기화시 좌표가 다르게 계산되기때문
-        for(int i=0; i<numbers.length; i++){
-            int key = numbers[i];
-            if(map.containsKey(key)){
-                String val = map.get(key);
-                if("L".equals(val)){
-                    lcur = key;
-                }else{
-                    rcur = key;
-                }
-                answer += val;
+        for(int n : numbers){
+            if(n==0) n = 11;
+            // System.out.println(left + " " + right + " " + n);
+            if(n == 1 || n == 4 || n == 7){
+                sb.append("L");
+                left = n;
+            }else if(n == 3 || n == 6 || n == 9){
+                sb.append("R");
+                right = n;
             }else{
-               // 가운데 열이면 거리 계산 
-                int leftDist = dist(lcur, key);
-                int rightDist = dist(rcur, key);
-                
-                if( rightDist > leftDist){
-                    answer += "L";
-                    lcur = key;
-                }else if(rightDist < leftDist){
-                    answer += "R";
-                    rcur = key;
+                if(chk(n)){
+                    sb.append("L");
+                    left = n;
                 }else{
-                    // 같은 경우 hand 기준
-                    if(hand.equals("left")){
-                        answer += "L";
-                        lcur = key;
-                    }else{
-                        answer += "R";
-                        rcur = key;
-                    }
+                    sb.append("R");
+                    right = n;
                 }
             }
         }
-        
-        return answer;
+        return sb.toString();
     }
+    
+    public boolean chk(int n){
+        boolean ck = false;
+        int leftDistance = Math.abs((left-1)/3 - (n-1)/3) 
+                        + Math.abs((left-1)%3 - (n-1)%3);
+        int rightDistance = Math.abs((right-1)/3 - (n-1)/3) 
+                        + Math.abs((right-1)%3 - (n-1)%3);
+                                     
+        if(leftDistance < rightDistance){
+            ck = true;
+        }else if(leftDistance > rightDistance){
+            ck = false;
+        }else {
+            if(hand.equals("left")){
+                ck = true;
+            }
+        }
+        
+        return ck;
+    }
+    
 }
