82034f832d59fc5344e68143dcc867fa11ad54f2
hyen20719 <hyen20719@gmail.com>
2026-02-11 17:00:17 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 1.57 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : win_nums){
+            set.add(n);
+        }
+        
+        int sameCnt = 0;
+        int zeroCnt = 0;
+        for(int lotto : lottos){
+            if(lotto == 0) zeroCnt++;
+            if(set.contains(lotto)) sameCnt++;
+        }
+        
+        int lank = 6;
+        int worst = sameCnt==0?6 :0;
+        for(int i=1; i<7; i++){
+            if(sameCnt == i){
+                worst = lank;
+                break;
+            }
+            lank--;
+        }
+        // System.out.println(lank);
+        // System.out.println(zeroCnt);
+        int best = lank-zeroCnt <1 ? 1 :lank-zeroCnt;
+        if(lank == 0 && zeroCnt == 0) best = 6;
+        answer[0] =best;
+        answer[1] = worst;
+        
+        return answer;
+    }
+}
