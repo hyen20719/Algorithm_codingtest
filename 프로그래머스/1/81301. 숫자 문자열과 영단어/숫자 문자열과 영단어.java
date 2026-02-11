56452009654357601066405c7532dc9442d4b868
hyen20719 <hyen20719@gmail.com>
2026-02-11 17:10:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,37 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        Map<String, Integer> map = new HashMap<>();
-        map.put("zero", 0);
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-        map.put("four", 4);
-        map.put("five", 5);
-        map.put("six", 6);
-        map.put("seven", 7);
-        map.put("eight", 8);
-        map.put("nine", 9);
-        
-        StringBuilder sbNum = new StringBuilder(); // 정답 담아놓는 sb
-        StringBuilder sb = new StringBuilder(); // 문자열 체크
-        
-        char[] crr = s.toCharArray();
-        for(char c : s.toCharArray()){
-            if(Character.isDigit(c)){ // 숫자인지
-                sbNum.append(c);
-                continue;
-            }
-            
-            sb.append(c);
-            if(map.containsKey(sb.toString())){
-                sbNum.append(map.get(sb.toString()));
-                sb.setLength(0);
-            }
-        }
-        
-        return Integer.parseInt(sbNum.toString());
+        s = s.replaceAll("zero","0")
+            .replaceAll("one","1")
+            .replaceAll("two","2")
+            .replaceAll("three","3")
+            .replaceAll("four","4")
+            .replaceAll("five","5")
+            .replaceAll("six","6")
+            .replaceAll("seven","7")
+            .replaceAll("eight","8")
+            .replaceAll("nine","9");
+        return Integer.parseInt(s);
     }
 }
